d8882d25d2d2a946116e7bf6cb9784b92ec46bf5
swiegers <srwiegers@gmail.com>
2017-04-01 11:00:31 -0400

    Add files via upload

---
+++ Hall_Effect_Sensor.ino
@@ -0,0 +1,34 @@
+int magPin = A0; //analog output pin
+int magData;  //analog output data
+int activationLED = 8; //LED used as flag
+void setup() {
+  pinMode(magPin, INPUT);
+  pinMode(activationLED, OUTPUT);
+  digitalWrite(activationLED, LOW);
+  Serial.begin(300); //low baud rate so I could follow it
+}
+
+void loop() {
+  magData= analogRead(magPin); //assigning the data from the pin to the variable
+  if ( 510 > magData || magData > 521) //range to set off flag
+      digitalWrite(activationLED, HIGH); 
+  else 
+      digitalWrite(activationLED,LOW);
+  Serial.println(magData);
+}
+/*
+int magPin = A0; //analog output pin
+int magData;  //analog output data  
+void setup() {
+  pinMode(magPin, INPUT);
+}
+int Hallflag()
+{
+    magData= analogRead(magPin); //assigning the data from the pin to the variable
+  if ( 510 > magData || magData > 521) //range to set off flag
+     magflag = 1;
+  else 
+     magflag = 0; 
+}
+*/
+
---
+++ IR_Sensor.ino
@@ -0,0 +1,25 @@
+const int IRsensor = 0;
+int IRread = 0;
+
+int A = 65;
+int E = 69;
+int I = 73;
+int O = 79;
+void setup() {
+  pinMode(IRsensor, INPUT);
+  Serial.begin(2400);
+}
+
+void loop() {
+  Serial.print(" IR Sensor: ");
+  Serial.available();
+  IRread = Serial.read();
+  if(IRread == A)
+    Serial.println("A");
+  if(IRread == E)
+    Serial.println("E");
+  if(IRread == I)
+    Serial.println("I");
+  if(IRread == O)
+    Serial.println("O");
+}
